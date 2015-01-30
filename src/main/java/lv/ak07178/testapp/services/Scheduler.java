6db56f5705c935a19336a46872e150e63eee962d
Kirill Afanasjev <kirill.afanasjev@odnoklassniki.ru>
2015-01-30 08:59:49 +0200

    scheduler

--- src/main/java/lv/ak07178/testapp/services/Scheduler.java
+++ src/main/java/lv/ak07178/testapp/services/Scheduler.java
@@ -1,19 +1,22 @@
 package lv.ak07178.testapp.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-/**
- * @author <a href="mailto:kirill.afanasjev@odnoklassniki.ru">Kirill Afanasjev</a>
- */
 @Component
 public class Scheduler {
 
+    private static final Logger log = LoggerFactory.getLogger(Scheduler.class);
+
+    @Autowired
+    private UserService userService;
+
     @Scheduled( fixedDelay = 1000 )
     public void expireArchive() {
-        System.out.println("running");;
+        log.info("Current user count is " + userService.getUserCount());
     }
-
-
 
 }
